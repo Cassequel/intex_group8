509b5d76bb9d2201c40cd76163c1dcb90606cd3c
jakejwright27 <mrjdawg@byu.edu>
2025-12-01 14:02:25 -0700

    added 418 code

--- index.js
+++ index.js
@@ -122,3 +122,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.render('public/landing');
 });
+
+app.get('/teapot', (req, res) => {
+    res.status(418).render('teapot');
+  });
