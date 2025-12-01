76ae3dff560e506603dbb21e31bce7fbf65ebed2
jakejwright27 <mrjdawg@byu.edu>
2025-12-01 11:05:57 -0700

    add knex

--- index.js
+++ index.js
@@ -17,10 +17,20 @@ app.use(session({
     saveUninitialized: false
 }));
 
-const knexConfig = require("./knexfile");
-const environment = process.env.NODE_ENV || "development";
-const knex = require("knex")(knexConfig[environment]);
+// const knexConfig = require("./knexfile");
+// const environment = process.env.NODE_ENV || "development";
+// const knex = require("knex")(knexConfig[environment]);
 
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host : process.env.RDS_HOST_NAME || "localhost",
+        user : process.env.RDS_USER_NAME || "postgres",
+        password : process.env.RDS_USER_PASSWORD || "admin",
+        database : process.env.RDS_DB_NAME || "foodisus",
+        port : process.env.RDS_PORT || 5434  // PostgreSQL 16 typically uses port 5434
+    }
+});
 // Root directory for static images
 const uploadRoot = path.join(__dirname, "images");
 // Sub-directory where uploaded profile pictures will be stored
