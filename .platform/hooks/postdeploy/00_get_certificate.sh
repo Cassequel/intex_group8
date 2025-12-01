3e226c4180cfa86d23d4b6e6eefa454dc4053c95
jakejwright27 <mrjdawg@byu.edu>
2025-12-01 11:35:20 -0700

    cert bug fix

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot -n -d ella-rises.com --nginx --agree-tos --email jakewright989@gmail.com
+sudo certbot -n -d http://ellarises2-8-env.eba-agwgxmmh.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email jakewright989@gmail.com
