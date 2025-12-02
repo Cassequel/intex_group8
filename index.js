411a753b7791f7b661371225e5f1723a961f0f6f
jakejwright27 <mrjdawg@byu.edu>
2025-12-01 17:15:21 -0700

    merge migrations

--- index.js
+++ index.js
@@ -24,16 +24,6 @@ const knexConfig = require("./knexfile");
 const environment = process.env.NODE_ENV || "development";
 const knex = require("knex")(knexConfig[environment]);
 
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-        host : process.env.RDS_HOST_NAME || "localhost",
-        user : process.env.RDS_USER_NAME || "postgres",
-        password : process.env.RDS_USER_PASSWORD || "wiztec12",
-        database : process.env.RDS_DB_NAME || "ellarises",
-        port : process.env.RDS_PORT || 5432  // PostgreSQL 16 typically uses port 5434
-    }
-});
 // Root directory for static images
 const uploadRoot = path.join(__dirname, "images");
 // Sub-directory where uploaded profile pictures will be stored
